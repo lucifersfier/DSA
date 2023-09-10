1db086b2cecb14151370529ca58940014b0dd433
Lucifersfier <nssrivas@gmail.com>
2023-09-10 08:42:16 +0530

    commit the array file

--- File1.py
+++ File1.py
@@ -1,1 +1,10 @@
-print("Hello")
+def fact(n):
+    if n<=0:
+        return 1                   
+    return n*fact(n-1)
+print(fact(6))
+''' Methods like this which are calling themselves recursively count in the space so it is going to be
+added in the memory to be remembered to come back to the last call'''             
+
+    
+        
