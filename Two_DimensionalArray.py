d1c427ff4bf03d81cec43e9c798d8adcee83fd9b
Lucifersfier <nssrivas@gmail.com>
2023-09-13 01:43:25 +0530

    Accessing the the dimensionaL ARRay element will start tommorow

--- Two_DimensionalArray.py
+++ Two_DimensionalArray.py
@@ -2,3 +2,18 @@ import numpy as np
 tda = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 print(tda)
 #time complexity of this operation is O(m*n)
+ntda = np.insert(tda,0,[[4,3,2,1]],axis=1) #here if you want add column then axis =1 otherwise for row axis = 0 
+print(ntda) #2nd position in np.insert(tda,0,[[4,3,2,1]],axis=1) tells about the position where you want to insert the column or row 
+
+
+#******************************#
+
+#by using append function we can app new row or column in the given array
+
+t_da = np.array([[9,8,7,6],[5,4,3,2]])
+nt_da = np.append(t_da,[[2,1,0,0]],axis= 0)
+print(nt_da)
+
+#Accessing an element of two dimensional array
+
+
