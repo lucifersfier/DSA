4d77f6afaa848c043280eb305960607a6c208072
Lucifersfier <nssrivas@gmail.com>
2023-09-13 21:54:41 +0530

    performed two operations acessing and searching today

--- Two_DimensionalArray.py
+++ Two_DimensionalArray.py
@@ -15,5 +15,39 @@ nt_da = np.append(t_da,[[2,1,0,0]],axis= 0)
 print(nt_da)
 
 #Accessing an element of two dimensional array
+def AcessElements(array,rowindex,colindex):
+    if rowindex>=len(array) and colindex>=len(array[0]):
+        print("Incorrect Index")
+    else:
+        print(array[rowindex][colindex])
+print("Acessing the element")
+AcessElements(tda,1,2)  # time complexity is O(1)
 
+#************************#
+print("traversing through two dimensional array")
+def Travrsing(array):
+    for i in range(len(array)):
+        for j in range ( len(array[0])):
+            print(array[i][j])
+Travrsing(tda)  # Time complexity will be O(mn) & space complexity will be O(1) 
+
+#*************************************#
+print("Searching for an element in two dimensional array")
+def Serarching(array,element):
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            if(array[i][j]==element):
+                print("found"+"",i,j)
+
+                
+Serarching(tda,13)
+
+#****************************************#
+
+
+
+
+    
+    
+    
 
