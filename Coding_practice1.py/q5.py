17d58132b8a74f777d68c878af9d7073f2dcf534
Lucifersfier <nssrivas@gmail.com>
2023-09-23 11:51:05 +0530

    Optimised solution of q5

--- Coding_practice1.py/q5.py
+++ Coding_practice1.py/q5.py
@@ -3,13 +3,24 @@
 '''Example
 myList2D= [[1,2,3],[4,5,6],[7,8,9]]  
 diagonal_sum(myList2D) # 15'''
-
+'''
 def diagonal_sum(matrix):
     summ=0
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             if(i==j):
                 summ+=matrix[i][j]
+    return summ
+myList2D= [[1,2,3],[4,5,6],[7,8,9]]  
+print(diagonal_sum(myList2D))
+l=[[3,2,1,5],[4,6,5,9],[8,9,7,10],[11,23,45,54]]
+print(diagonal_sum(l))'''
+
+#optimised solution is below now 
+def diagonal_sum(matrix):
+    summ=0
+    for i in range(len(matrix)):
+        summ+=matrix[i][i]
     return summ
 myList2D= [[1,2,3],[4,5,6],[7,8,9]]  
 print(diagonal_sum(myList2D))
