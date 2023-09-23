e40b12f757bb6b0a7bfc19a35db097f83bd41e96
Lucifersfier <nssrivas@gmail.com>
2023-09-23 11:08:28 +0530

    Question 4 optimal solution

--- Coding_practice1.py/q4.py
+++ Coding_practice1.py/q4.py
@@ -1,6 +1,6 @@
 #Max Product
 import numpy as np
-
+'''
 def maxx(arr):
     maxi=0
     for i in range(len(arr)):
@@ -10,4 +10,18 @@ def maxx(arr):
     return maxi
 l=np.array([1,8,2,3,4,5,6,7])
 print(maxx(l))
+#here  time complexity is O(n*n)
+'''
+#optimise solution will be 
 
+def max_product(arr):
+    max1,max2=0,0
+    for i in arr:
+        if (i>max1):
+            max1 = i
+    for i in arr:
+        if(i>max2 and i<max1):
+            max2=i
+    return max1*max2
+l=np.array([1,12,2,3,10,5,89,7])
+print(max_product(l))
