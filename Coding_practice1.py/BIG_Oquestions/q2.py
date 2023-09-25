a89ebce3ce8ba8985ca8a3cc6683b0ce0bbd20d2
Lucifersfier <nssrivas@gmail.com>
2023-09-25 15:58:05 +0530

    q2 is completed now

--- Coding_practice1.py/BIG_Oquestions/q2.py
+++ Coding_practice1.py/BIG_Oquestions/q2.py
@@ -1,9 +1,9 @@
 '''What is the run time of the below code'''
 
 def printpairs(array):
-    for i in array:     #-------------------------->>> O(n)
+    for i in array:     #-------------------------->>> O(n**2)
         for j in array: #------------------------------>>> O(n)
-            print(str(i)+","+str(j))
+            print(str(i)+","+str(j)) #---------------------->>>O(1)
             
 '''there is a nested loop which means operation will be performed for each n, n times 
 Time Complexity Will be O(n**2)'''
