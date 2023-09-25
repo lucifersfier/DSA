9356423f69e499e2218612a0ae6201d915c0710f
Lucifersfier <nssrivas@gmail.com>
2023-09-25 16:10:17 +0530

    q3 is completed

---
+++ Coding_practice1.py/BIG_Oquestions/q3.py
@@ -0,0 +1,12 @@
+'''what is the run time of the below code'''
+def printunorderedpair(arrayA,arrayB):
+    for i in range(len(arrayA)):     #---------------> O(ab)
+        for j in range(len(arrayB)): #------------------> O(b)
+            if(arrayA[i]<arrayB[j]): #---------------------> O(1)
+                print(str(arrayA[i])+","+str(arrayB[j]))
+                
+x=[1,2,3,4,5,6,78,9]
+y=[3,4,5,67,2,3,4,5]
+print(printunorderedpair(x,y))
+
+'''Time Complexity will be O(ab) if the length of arrayA is 'a' and the length of arrarB is 'b' '''
