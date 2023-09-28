65d396b7f2d5ccb5255a39d14e0accc9ed9aed34
Lucifersfier <nssrivas@gmail.com>
2023-09-28 11:38:26 +0530

    DEleting and removing method in an dictionary

--- dictionary.py
+++ dictionary.py
@@ -36,8 +36,40 @@ def traverseDict(dictt):
     for key in dictt:   #------------------------>> O(n)
         print(key,dictt[key]) #------------------------>> O(1)
 traverseDict(dictt)
-'''Time and space complexity of the just above function is O(1) '''
+'''Time and space complexity of the just above function is O(n) and O(1) '''
+
+#Search for an element in a Dictionary 
+
+def SearchDictt(dict,value):
+    for key in dict:
+        if(dict[key]==value):
+            return key, value
+    return "The Value doesn't exist"
+print(SearchDictt(dictt,22))
+print(SearchDictt(dictt,23))
+
+'''Time and space complexity for just above the function is O(n) and O(1)'''
+
+#Remove/Delete an element from the dictionary
+
+del dictt['address']
+print(dictt)
+'''time complexity for deleting an element from the dictionary is O(1) because it uses hash table'''
+'''Space complexity is also O(1)'''
+'''another method to remove a key-value pair from a dictioanry is pop method
+this method is also returns the value associated with the key
+and if the key is not found, it returns a default value that is provided otherwise it's going to raise 
+key error'''
 
 
+remv = dictt.pop('gender')
+print(remv)
+print(dictt)
 
+'''if we write like this 'remv = dictt.pop('gen',None)' and we didnt found any key or value 
+in the dictionary then it will not give an error so always make sure to provide parameters so that program can run smoothly'''
+#remv = dictt.popitem() then it will delete the last key value pair from the dictionary
+#Clear method removes all key value pairs from the dictionary dictt.clear() and then print(dictt) 
+# it will show empty dictionary Time and space complexity is O(n) and O(1) 
+'''Time and Space Complexity is O(1)'''
 
