c284c217d732d68bc9ca2ec520447d73517153dd
Lucifersfier <nssrivas@gmail.com>
2023-09-28 11:15:06 +0530

    Travering through a dictionary

--- dictionary.py
+++ dictionary.py
@@ -14,3 +14,30 @@ print(eng_sp2)   #Space and Time Complexity will be O(n)
 eng_sp3_list = [('one','uno'),('two','dos'),('three','tres')]
 eng_sp3 = dict(eng_sp3_list)
 print(eng_sp3)
+
+#Dictionary in a memory
+
+'''A hash table is a way of doing key value lookups you store the values in an array
+and then use a hash function to find the index of the array cell that corresponds to your key-value pair'''
+
+#Insert/Update an element in an array
+
+dictt = {'name':'nittyansh','age':22,'gender':'male'}
+dictt['name']='shivansh'
+print(dictt)
+
+dictt['address']='Ghaziabad'
+print(dictt)
+'''Time and space complexity is O(1)'''
+
+#Traversing through a dictionary 
+
+def traverseDict(dictt):
+    for key in dictt:   #------------------------>> O(n)
+        print(key,dictt[key]) #------------------------>> O(1)
+traverseDict(dictt)
+'''Time and space complexity of the just above function is O(1) '''
+
+
+
+
