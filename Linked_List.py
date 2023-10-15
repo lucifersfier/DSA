1b1aeee58bf40c6b0a307d0bdada3da04ef766b0
Lucifersfier <nssrivas@gmail.com>
2023-10-16 03:12:54 +0530

    Starting Linked List

---
+++ Linked_List.py
@@ -0,0 +1,4 @@
+#)-Linked list is a form of sequential collection and it doesn't have to be in order
+'''A Linked list is made up of independent nodes that may contain any type of data and
+   each node has a reference to the next node in the link'''
+   
